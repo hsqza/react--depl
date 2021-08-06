acfba578d228e08379df087359dea562098d0c41
askuza <anna.skuza7@gmail.com>
2021-08-06 15:15:24 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  console.log(process.env.REACT_APP_MY_API_KEY);
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
